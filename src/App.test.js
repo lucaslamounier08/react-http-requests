1d0fd7d9d27320929fe964e4eb8e475c8076820c
lucas.faria <lucas.faria@ambevtech.com.br>
2023-04-11 19:01:16 -0300

    removed github action node

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders hi! link', () => {
+test("renders hi! link", () => {
   render(<App />);
   const linkElement = screen.getByText(/hi!/i);
   expect(linkElement).toBeInTheDocument();
